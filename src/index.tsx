7e7f5becb3268f05f96959ab2f2e8de264e4db20
Dominic Bisschop <dominicbisschop@hotmail.com>
2022-10-06 16:17:04 +0200

    Possible fix for GH Pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <BrowserRouter>
+    <BrowserRouter basename="/3D-CSS-Playground">
       <App />
     </BrowserRouter>
   </ThemeProvider>,
